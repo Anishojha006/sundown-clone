0e3d1471e11c4a26b166af2b71032681d5321a51
Anishojha006 <anishojha1512@gmail.com>
2026-06-08 08:34:01 +0530

    working on slider

--- script.js
+++ script.js
@@ -1,45 +1,39 @@
 
-// const scroll = new LocomotiveScroll({
-//   el: document.querySelector('#main'),
-//   smooth: true
-// })
-let imgBox = document.querySelector("#fixed-image");
-let elem = document.querySelectorAll(".elem");
-let container = document.querySelector("#elem-container");
-elem.forEach((e) => {
-  console.log(e);
+function Page4andOtherAnimation() {
+  //   const scroll = new LocomotiveScroll({
+  //   el: document.querySelector('#main'),
+  //   smooth: true
+  // })
+  let imgBox = document.querySelector("#fixed-image");
+  let elem = document.querySelectorAll(".elem");
+  let container = document.querySelector("#elem-container");
+  elem.forEach((e) => {
+    e.addEventListener("mouseenter", function () {
+      imgBox.innerHTML = "";
+      let imgLink = e.getAttribute("data-image");
+      let imgTag = document.createElement("img");
+      imgTag.setAttribute("src", imgLink);
+      imgBox.appendChild(imgTag);
 
-  e.addEventListener("mouseenter", function () {
-    imgBox.innerHTML = "";
-    let imgLink = e.getAttribute("data-image");
-    console.log(imgLink);
-    let imgTag = document.createElement("img");
-    imgTag.setAttribute("src", imgLink);
-    imgBox.appendChild(imgTag);
-    console.log(imgBox);
-
-  });
-})
-console.log(imgBox);
-
-container.addEventListener("mouseenter", function () {
-  imgBox.style.display = "flex"
-
-})
-container.addEventListener("mouseleave", function () {
-  imgBox.style.display = "none";
-})
-
-function SwipperAnimation(){
-    var swiper = new Swiper(".mySwiper", {
-      slidesPerView: "auto",
-      centeredSlides: true,
-      spaceBetween:100,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
     });
+  })
+
+  container.addEventListener("mouseenter", function () {
+    imgBox.style.display = "flex"
+
+  })
+  container.addEventListener("mouseleave", function () {
+    imgBox.style.display = "none";
+  })
+}
+Page4andOtherAnimation();
+
+function SwipperAnimation() {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 100,
+  });
 }
 SwipperAnimation();
 
