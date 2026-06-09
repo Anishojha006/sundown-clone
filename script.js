06081bc66e0dd0102900606040e035a50d950d2d
Anishojha006 <anishojha1512@gmail.com>
2026-06-09 22:35:17 +0530

    working on slider

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function SwipperAnimation() {
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
     centeredSlides: true,
-    spaceBetween: 100,
+    spaceBetween: 60,
   });
 }
 SwipperAnimation();
